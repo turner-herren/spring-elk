315b324a4db52e61356ba98e81a925157fd00c17
donggyukim <zeedoutladzz@gmail.com>
2023-02-19 19:03:21 +0900

    elk init

---
+++ src/test/kotlin/com/elk/SpringElkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.elk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringElkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
